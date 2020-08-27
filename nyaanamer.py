839ead3828a1f2c5dbbb107dde5b9e0abc83c3a4
euphonicazure <38431726+euphonicazure@users.noreply.github.com>
2020-08-27 18:40:18 +0600

    rename() is now a generator that returns original name and renamed name

--- nyaanamer.py
+++ nyaanamer.py
@@ -2,37 +2,47 @@ import os
 import re
 import path_handler
 
+EXTENSION_REGEX = r"\.?(\w+)"
+
 def only_ignore_handler(path, only="", ignore=""):
     """Returns a list of files and directories in a path after filtering them according to the
-    conditions in the argument. "only" has more priority than "ignore". By default they're empty strings"""
+    conditions in the argument. "only" has more priority than "ignore". By default they're empty strings
+    """
     files = os.listdir(path)
     if only:
-        only = [c if c.startswith(".") else "."+c for c in only.split()]       # keeping this as a list for mutability
-        if ".directory" in only:
-            only.remove(".directory")
+        only = ["."+extension for extension in re.findall(EXTENSION_REGEX, only)]
+        if '.directory' in only:
+            only.remove('.directory')   # Removing ".directory" to ensure smooth filtering of files with extensions
             directories = [f for f in files if os.path.isdir(os.path.join(path, f))]    # sorts out the directories
             files = [f for f in files if f.endswith(tuple(only))] + directories
         else:
             files = [f for f in files if f.endswith(tuple(only))]
 
     elif ignore:
-        ignore = [c if c.startswith(".") else "."+c for c in ignore.split()]
-        if ".directory" in ignore:
-            ignore.remove(".directory")
-            directories = [f for f in files if os.path.isdir(os.path.join(path, f))]
+        ignore = ["."+extension for extension in re.findall(EXTENSION_REGEX, ignore)]
+        if '.directory' in ignore:
+            ignore.remove('.directory') # Removing ".directory" to ensure smooth filtering of files with extensions
+            directories = [f for f in files if os.path.isdir(os.path.join(path, f))]    # sorts out the directories
             files = [f for f in files if not f.endswith(tuple(ignore)) and f not in directories]
         else:
             files = [f for f in files if not f.endswith(tuple(only))]
 
     return files
 
-def rename(path, regex, replacewith, only="", ignore=""):
-    """Renames the files/directories filtered by only_ignore_handler by replacing the regex matches with
-    provided [replacewith] argument"""
+def rename(path, regex, replacewith, **kwargs):
+    """Yields a tuple of original name and renamed name of the files filtered according to regex
+    in a specific path provided in the argument. Keyword arguments are only and ignore which are both empty strings
+    by default.
+    """
+    # PLAN: Add safe mode so that regex doesn't mess with extension
     gex = re.compile(regex)
-    files = only_ignore_handler(path, only, ignore)
-
+    files = only_ignore_handler(path, kwargs.get('only', ""), kwargs.get('ignore', ""))
+    
     for name in files:
         rename = gex.sub(replacewith, name)
-        os.rename((os.path.join(path, name)), (os.path.join(path, rename)))
+        original, renamed = (os.path.join(path, name)), (os.path.join(path, rename))
+        yield original, renamed
 
+
+# for x, y in rename(r"D:\Programming\Training\test", r"\d", "LOL", only=".directory"):
+#     print(x, "---->", y)
