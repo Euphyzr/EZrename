c31c579cad55b8bda06fba884370131407b724a1
euphonicazure <38431726+euphonicazure@users.noreply.github.com>
2020-09-06 11:37:48 +0600

    Fixed '--regex-add' bug and tried to organize the code a bit more

--- EZrename.py
+++ EZrename.py
@@ -3,6 +3,7 @@ import re
 
 EXTENSION_REGEX = r"\.?(\w+)"
 
+
 def only_ignore_handler(path, only="", ignore=""):
     """Returns a list of files and directories in a path after filtering them according to the
     conditions in the argument. "only" has more priority than "ignore". By default they're empty strings
@@ -12,23 +13,23 @@ def only_ignore_handler(path, only="", ignore=""):
         # re.findall() expects string or byte-like object but parser returns list. so, " ".join() is called
         only = ["."+extension for extension in re.findall(EXTENSION_REGEX, " ".join(only))]
         if '.directory' in only:
-            only.remove('.directory')   # Removing ".directory" to ensure smooth filtering of files with extensions
-            directories = [f for f in files if os.path.isdir(os.path.join(path, f))]    # sorts out the directories
+            only.remove('.directory')  # Removing ".directory" to ensure smooth filtering of files with extensions
+            directories = [f for f in files if os.path.isdir(os.path.join(path, f))]  # sorts out the directories
             files = [f for f in files if f.endswith(tuple(only))] + directories
         else:
             files = [f for f in files if f.endswith(tuple(only))]
 
     elif ignore:
-        # re.findall() expects string or byte-like object but parser returns list. so, " ".join() is called
         ignore = ["."+extension for extension in re.findall(EXTENSION_REGEX, " ".join(ignore))]
         if '.directory' in ignore:
-            ignore.remove('.directory') # Removing ".directory" to ensure smooth filtering of files with extensions
-            directories = [f for f in files if os.path.isdir(os.path.join(path, f))]    # sorts out the directories
+            ignore.remove('.directory')
+            directories = [f for f in files if os.path.isdir(os.path.join(path, f))]
             files = [f for f in files if not f.endswith(tuple(ignore)) and f not in directories]
         else:
             files = [f for f in files if not f.endswith(tuple(ignore))]
 
     return files
+
 
 def renamed_name(path, replacewith, regex, **kwargs):
     """Returns a list consisting tuple of original name and renamed name of the files filtered according to regex
@@ -45,57 +46,66 @@ def renamed_name(path, replacewith, regex, **kwargs):
 
     return original_and_renamed_name
 
+
 if __name__ == "__main__":
     import sys
     import argparse
     import path_handler
     import json_config_handler
 
+    JSON_INDENT = 4
+    PRESETS_LIMIT = 5
     NO_REGEX_MESSAGE = """
     No default regex is setted up. Use -d/--regex-default <regex> to setup a default regex.
     Alternatively, use -r/--regex <regex> to use a regex without a default one"""
-    JSON_INDENT = 4
-    PRESETS_LIMIT = 5
-    RESTRICTION_MESSAGE = "Cannot have more than {} presets. Remove presets with -rd/--regex-delete [name]".format(PRESETS_LIMIT)
-    
-    CONFIG_FLAGS = ['-d', '-ra', '-rd', '-rl', '-rc', '-u', '--regex-default' \
-        '--regex-add', '--regex-delete', '--regex-list', '--regex-clear', '--undo']
-    def is_flags_in_argv(flags):
+
+    CONFIG_FLAGS = [
+        '-d', '-ra', '-rd', '-rl', '-rc', '-u', '--regex-default', '--regex-add',
+        '--regex-delete', '--regex-list', '--regex-clear', '--undo'
+        ]
+    def is_no_config_flags(flags=CONFIG_FLAGS):
         for flag in flags:
             if flag in sys.argv:
-                return True
+                return False
                     
     phandler = path_handler.PathHandler(invalid_turns=3, sys_exit=True)
     config_path = os.path.join((os.path.dirname(os.path.realpath(__file__))), "config.json")
     j = json_config_handler.JsonConfigHandler(config_path)
-
-    def restriction(msg=RESTRICTION_MESSAGE):
-        sys.exit(msg)
+    
+    def restriction():
+        sys.exit("Cannot have more than {} presets. Remove presets with -rd/--regex-delete [name]".format(PRESETS_LIMIT))
     j.on_restriction(restriction)
 
+    
     parser = argparse.ArgumentParser(description="Bulk renames files and directories with handy options.")
-    parser.add_argument('-p', '--path', help="path to files or directories", required=not is_flags_in_argv(CONFIG_FLAGS))
-    parser.add_argument('-w', '--replacewith', help="matched regular expression pattern are replaced by this",\
-        required=not is_flags_in_argv(CONFIG_FLAGS))
+    parser.add_argument('-p', '--path', help="path to files or directories", required=is_no_config_flags())
+    parser.add_argument('-w', '--replacewith', help="matched regular expression pattern are replaced by this",
+                                            required=is_no_config_flags())
+    
     custom_regex = parser.add_mutually_exclusive_group()
     custom_regex.add_argument('-r', '--regex', help="matches regular expression pattern to replace it")
-    custom_regex.add_argument('-pr', '--preset-regex', help="finds regex by name from presets to replace it. Has less \
-        priority than -r/--regex")
+    custom_regex.add_argument('-pr', '--preset-regex', help="finds regex by name from presets to replace it.")
+
     parser.add_argument('-u', '--undo', action='store_true', help="undos the batch of rename")
     parser.add_argument('-q', '--quiet', action='store_true', help="doesn't display rename status")
+    
     only_ignore = parser.add_mutually_exclusive_group()
     only_ignore.add_argument('-o', '--only', nargs='*', help="only matches for provided extension(s) as well as directory")
     only_ignore.add_argument('-i', '--ignore', nargs='*', help="ignores provided extension(s) as well as directory")
+    
     config_args = parser.add_argument_group('config')
     config_args.add_argument('-d', '--regex-default', nargs='?', const=True, help="sets up a default regex to match by")
-    config_args.add_argument('-ra', '--regex-add', nargs=2, metavar=('NAME', 'REGEX'), help="adds regex by name in regex presets.\
-         Multiworded name needs to be quoted")
-    config_args.add_argument('-rd', '--regex-delete', nargs='+', metavar='NAME', help="deletes one/multiple regex by it's \
-        name from regex presets. Multiworded name needs to be quoted. Remove default with 'default'")
     config_args.add_argument('-rl', '--regex-list', action='store_true', help="lists all regex presets")
-
+    
+    config_args.add_argument('-ra', '--regex-add', nargs=2, metavar=('NAME', 'REGEX'),
+                                                help="adds regex by name in regex presets. Multiworded name needs to be quoted")
+    
+    config_args.add_argument('-rd', '--regex-delete', nargs='+', metavar='NAME',
+                                                    help=("deletes one/multiple regex by it's name from regex presets. Multiworded"
+                                                    " name needs to be quoted. Remove default with 'default'"))
     args = parser.parse_args()
 
+    
     def rename(source, undo=False, quiet=args.quiet, exception_counter=1):
         updated_last_changes = {}
 
@@ -108,13 +118,15 @@ if __name__ == "__main__":
                 renamed = renamed + " ({})".format(exception_counter)
                 os.rename(original, renamed)
                 exception_counter += 1
-            
+
             updated_last_changes.update({original: renamed})
+
             if not quiet:
                 print(original, "---->", renamed)
         
         j.json_data['last_changes'] = updated_last_changes
         j.dump(indent=JSON_INDENT)
+
 
     if args.path and args.replacewith:
         path = phandler.input_validator(args.path)
@@ -124,7 +136,7 @@ if __name__ == "__main__":
             try:
                 regex = j.json_data['presets'][args.preset_regex]
             except KeyError:
-                sys.exit("No preset exists by that name. -rl/--regex-list gives a list of all presets")
+                sys.exit("\nNo preset exists by that name. -rl/--regex-list gives a list of all presets")
         elif j.json_data['regex_default']:
             regex = j.json_data['regex_default']
         else:
